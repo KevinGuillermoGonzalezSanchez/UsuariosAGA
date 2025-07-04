6c287aacf72087857a6af81466df3ec3ff4ec1e3
anajim02 <anajimenezd02@hotmail.com>
2025-07-04 15:51:46 -0600

    rama de bl

--- BL/Usuario.cs
+++ BL/Usuario.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,10 @@ namespace BL
 {
     public class Usuario
     {
+        public string ML.Result AddLinQ(ML.Usuario usuario)
+        {
+
+
+        }
     }
 }
