6e6372be5fe357738e8c4125777aeb3879be14ef
AndreaSilva-8 <andreaalisc@outlook.com>
2025-07-04 15:10:05 -0600

    ML con sus propiedades

--- ML/Usuario.cs
+++ ML/Usuario.cs
@@ -8,5 +8,10 @@ namespace ML
 {
     public class Usuario
     {
+        public int IdUsuario { get; set; }
+        public string Nombre { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public string FechaNacimiento { get; set; }
     }
 }
