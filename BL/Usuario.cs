6d5a962e447dcae7b56cb56c7dbddc239ba65141
ETena <henrytbr21@gmail.com>
2025-07-04 18:09:54 -0600

    cambios

--- BL/Usuario.cs
+++ BL/Usuario.cs
@@ -9,27 +9,7 @@ namespace BL
     public class Usuario
     {
 
-        public static ML.Result GetAll()
-        {
-            ML.Result result = new ML.Result();
-
-            try
-            {
-                using (DL_EF.UsuariosAGAEntities context = new DL_EF.UsuariosAGAEntities())
-                {
-                    var listUsuarios = (from UsuarioDB in context.Usuarios
-                                        select new
-                                        {
-                                            IdUsuario = UsuarioDB.IdUsuario,
-                                            Nombre = UsuarioDB.Nombre,
-                                            ApellidoPaterno = UsuarioDB.ApellidoPaterno,
-                                            ApellidoMaterno = UsuarioDB.ApellidoMaterno,
-                                            FechaNacimiento = UsuarioDB.FechaNacimiento,
-
-                                        }).ToList();
-
-        }
-
+       
      
 
     }
