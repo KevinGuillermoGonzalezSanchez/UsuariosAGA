24dd55d2fc4225aa9ebf673bbd38591aed298f7d
AndreaSilva-8 <andreaalisc@outlook.com>
2025-07-07 10:46:50 -0600

    GetById y Update implementacion

--- PL_MVC/Controllers/UsuarioController.cs
+++ PL_MVC/Controllers/UsuarioController.cs
@@ -9,20 +9,44 @@ namespace PL_MVC.Controllers
     public class UsuarioController : Controller
     {
         // GET: Usuario
+        [HttpGet]
+        public ActionResult Formulario()
+        {
+            ML.Result result = new ML.Result();
+            ML.Usuario usuario = new ML.Usuario();
 
+            if (IdUsuario == 0)
+            {
+
+            }
+            else
+            {
+                var respuesta = GetByIdLinq((int)IdUsuario);
+
+                    if (respuesta.Correct = true)
+                    {
+                    usuario = (ML.Usuario)respuesta.Object;
+                    }
+            }
+        }
 
         [HttpPost]
         public ActionResult Formulario(ML.Usuario usuario)
         {
             ML.Result result = new ML.Result();
 
-            if (usuario.IdUsuario == 0) 
+            if (usuario.IdUsuario == 0)
             {
                 result = BL.Usuario.AddLinQ(usuario);
                 return RedirectToAction("GetAll");
             }
-            
-                return View();
+            else
+            {
+                result = BL.UpdateLinq(IdUsuario);
+                return RedirectToAction("GetAll");
+            }
+
+            return View();
         }
         public ActionResult Delete(int IdUsuario)
         {
